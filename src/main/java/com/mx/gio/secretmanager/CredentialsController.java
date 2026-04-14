a81bb2f532019b9c71ae0474ede48dc0d675bb96
Giovanni Launizar <gio.desarroller@gmail.com>
2026-04-14 10:30:31 -0600

    first

---
+++ src/main/java/com/mx/gio/secretmanager/CredentialsController.java
@@ -0,0 +1,34 @@
+package com.mx.gio.secretmanager;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CredentialsController {
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @GetMapping("/credentials")
+    public ResponseEntity<Map<String, String>> get() {
+
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("spring.datasource.username", username);
+        credentials.put("spring.datasource.password", password);
+        credentials.put("spring.datasource.url", url);
+
+
+        return ResponseEntity.ok(credentials);
+    }
+}
